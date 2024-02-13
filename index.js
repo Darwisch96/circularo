18d77299d11f2ef912696f66c27bb5d15ad38cd5
Darwisch96 <100080935+Darwisch96@users.noreply.github.com>
2024-02-13 17:13:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const path = require('path');
 
 const app = express();
 
-// Define a route to serve the content of index.html
+// // Define a route to serve the content of index.html
+// app.get('/', (req, res) => {
+//     // Read the content of the index.html file
+//     fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+//         if (err) {
+//             // If there's an error reading the file, return a 500 Internal Server Error
+//             res.status(500).send('Internal Server Error');
+//         } else {
+//             // Set the Content-Type header to indicate that the response contains HTML
+//             res.setHeader('Content-Type', 'text/html');
+//             // Send the content of the index.html file as the response body
+//             res.send(data);
+//         }
+//     });
+// });
+
+
 app.get('/', (req, res) => {
-    // Read the content of the index.html file
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-        if (err) {
-            // If there's an error reading the file, return a 500 Internal Server Error
-            res.status(500).send('Internal Server Error');
-        } else {
-            // Set the Content-Type header to indicate that the response contains HTML
-            res.setHeader('Content-Type', 'text/html');
-            // Send the content of the index.html file as the response body
-            res.send(data);
-        }
-    });
-});
-
-
-app.get('/hhh', (req, res) => {
     res.status(200).json({
         status: 'tttte'
     })
